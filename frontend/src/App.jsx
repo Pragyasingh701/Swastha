bce4136067a5c411eea11d864fc24b58d4533154
Pragyasingh701 <singhpragya701@gmail.com>
2026-07-21 13:40:41 +0530

    wip: local App.jsx changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Landing Page
+//landingpage
 import LandingPage from "./modules/landing/pages/LandingPage";
 
 // Authentication
 import Login from "./modules/authentication/Login";
 import Register from "./modules/authentication/Register";
 import ForgotPassword from "./modules/authentication/ForgotPassword";
-import ResetPassword from "./modules/authentication/ResetPassword";
-import VerifyOTP from "./modules/authentication/VerifyOTP";
 import RoleSelection from "./modules/authentication/RoleSelection";
-import DoctorLogin from "./modules/authentication/DoctorLogin";
-import DoctorRegister from "./modules/authentication/DoctorRegister";
 
 // Dashboard
 import Dashboard from "./modules/dashboard/pages/Dashboard";
+
 // Family
 import FamilyVault from "./modules/family/pages/FamilyVault";
 
@@ -29,17 +26,13 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         {/* Landing Page */}
-        <Route path="/" element={<LandingPage />} />
+  <Route path="/" element={<LandingPage />} />
 
         {/* Authentication */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/verify-otp" element={<VerifyOTP />} />
         <Route path="/role-selection" element={<RoleSelection />} />
-        <Route path="/doctor-login" element={<DoctorLogin />} />
-        <Route path="/doctor-register" element={<DoctorRegister />} />
 
         {/* Main App */}
         <Route path="/dashboard" element={<Dashboard />} />
